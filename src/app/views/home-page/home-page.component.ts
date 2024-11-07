c92bec7cec2687cb7d410c5c9763c654acf25ac9
heribertosanchez <desarrolladorerpsoftware9.itm@gmail.com>
2024-11-07 08:26:16 -0500

    Agrega componentes, servicios, modelos y vistas iniciales

---
+++ src/app/views/home-page/home-page.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomePageComponent {
+
+}
