8c0207307998e014b43e8e431b49b4d5ba5c07ec
Heri <hersac1@gmail.com>
2024-11-11 23:24:59 -0500

    Agrega variables para scss, agrega estilos para login, agrega clases iniciales para register

--- src/app/views/auth/login-page/login-page.component.ts
+++ src/app/views/auth/login-page/login-page.component.ts
@@ -1,5 +1,10 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { LoginRequest } from '../../../models/login-request';
 import { AuthService } from '../../../services/auth.service';
 import { catchError, map } from 'rxjs';
@@ -11,33 +16,34 @@ import { Router, RouterModule } from '@angular/router';
   standalone: true,
   imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrl: './login-page-responsive.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginPageComponent {
-
   private authService = inject(AuthService);
   private router = inject(Router);
   formLogin: FormGroup = new FormBuilder().group({
     email: ['', Validators.required],
-    contrasena: ['', Validators.required]
+    contrasena: ['', Validators.required],
   });
 
-
   async ingresar() {
-    if(this.formLogin.invalid) {
+    if (this.formLogin.invalid) {
       return;
     }
     const credenciales: LoginRequest = this.formLogin.value;
 
-    this.authService.login(credenciales).pipe(
-      map((token: TokenResponse) => {
-        sessionStorage.setItem('token', token.token);
-        this.router.navigate(['/']);
-      }),
-      catchError((error) => {
-        throw new Error(`Error al iniciar sesión: ${error}`)
-      }),
-    ).subscribe();
+    this.authService
+      .login(credenciales)
+      .pipe(
+        map((token: TokenResponse) => {
+          sessionStorage.setItem('token', token.token);
+          this.router.navigate(['/']);
+        }),
+        catchError((error) => {
+          throw new Error(`Error al iniciar sesión: ${error}`);
+        })
+      )
+      .subscribe();
   }
 }
