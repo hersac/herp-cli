1719ee384343b0c44333871aa8c4e1ee20270337
Heri <hersac1@gmail.com>
2025-01-03 22:37:38 -0500

    Implementa componente tabla, agrega estilos y muestra datos iterados por defecto, aun no se generan los template por celda ni por header

--- src/app/components/tabla/tabla.component.ts
+++ src/app/components/tabla/tabla.component.ts
@@ -1,23 +1,32 @@
+import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
-  ContentChild,
+  ContentChildren,
   Input,
+  QueryList,
   TemplateRef,
 } from '@angular/core';
 
 @Component({
   selector: 'app-tabla',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tabla.component.html',
-  styleUrl: './tabla.component.scss',
+  styleUrls: ['./tabla.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TablaComponent {
   @Input() headers: Array<any> = [];
   @Input() data: Array<any> = [];
   @Input() loading: boolean = false;
-  @ContentChild('headerTemplate') headerTemplate?: TemplateRef<any>;
-  @ContentChild('cellTemplate') cellTemplate?: TemplateRef<any>;
+  @ContentChildren(TemplateRef) templates: QueryList<TemplateRef<any>> | undefined;
+
+  getHeaderTemplate(headerKey: string): TemplateRef<any> | undefined {
+    return this.templates?.find((template: any) => template['id'] === headerKey);
+  }
+
+  getCellTemplate(headerKey: string): TemplateRef<any> | undefined {
+    return this.templates?.find((template: any) => template['id'] === `cell-${headerKey}`);
+  }
 }
